865c7a10151d9f935dec7a92993cba79d62483dd
OisheeBintey_Hoque <bintu3003@gmail.com.com>
2026-03-18 01:50:36 -0400

    slurm account name update

--- slurm_job.sh
+++ slurm_job.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64GB
 #SBATCH --time=12:00:00
-#SBATCH --account=<your_allocation>
+#SBATCH --account=gza5dr
 
 # Activate your conda env
 module load miniforge
