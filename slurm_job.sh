29ca95754d8f06ac6e883ce287e7d0124e76a7f9
OisheeBintey_Hoque <bintu3003@gmail.com.com>
2026-03-18 18:32:44 -0400

    changes

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=irrigation
-#SBATCH --output=%u-%j.out
-#SBATCH --error=%u-%j.err
+#SBATCH --output=slurm/%u-%j.out
+#SBATCH --error=slurm/%u-%j.err
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100:1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=64GB
-#SBATCH --time=12:00:00
-#SBATCH --account=gza5dr
+#SBATCH --gres=gpu
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=760GB
+#SBATCH --time=6:00:00
+#SBATCH --account=nssac_students
 
 # Activate your conda env
 module load miniforge
@@ -15,5 +15,15 @@ source activate /sfs/weka/scratch/gza5dr/IrrigationType_experiments/mutli_reason
 
 # Run from scratch (faster I/O than /home)
 cd /sfs/weka/scratch/gza5dr/IrrigationType_experiments/mutli_reasoning/ag_multimodal_reasoning
+# Redirect wandb to scratch (avoid home quota)
+export WANDB_DIR=/sfs/weka/scratch/gza5dr/wandb_tmp
+export WANDB_CACHE_DIR=/sfs/weka/scratch/gza5dr/wandb_cache
+export WANDB_DATA_DIR=/sfs/weka/scratch/gza5dr/wandb_data
 
-python scripts/train.py +experiment=step1_rgb_baseline
+
+python scripts/train.py model=deeplabv3plus_efficientnet_b3 data=rgb_single experiment_name=A4_dlv3_50_rgb_effe_dice_norm
+# python model=deeplabv3plus_efficientnet_b3 data=rgb_noise_reduced data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=B1_noise_rgb_effe_dice
+# python model=deeplabv3plus_efficientnet_b3 data=spectral_single data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=C1_spectral_effe_dice
+# python model=deeplabv3plus_efficientnet_b3 data=temporal data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=C1_temporal_effe_dice
+
+
