d09dcf3942d2c5d852ac75f9633848bf094852a7
OisheeBintey_Hoque <bintu3003@gmail.com.com>
2026-03-18 21:49:00 -0400

    .sh file added to gitignore

--- slurm_job.sh
+++ slurm_job.sh
@@ -22,8 +22,8 @@ export WANDB_DATA_DIR=/sfs/weka/scratch/gza5dr/wandb_data
 
 
 python scripts/train.py model=deeplabv3plus_efficientnet_b3 data=rgb_single experiment_name=A4_dlv3_50_rgb_effe_dice_norm
-# python model=deeplabv3plus_efficientnet_b3 data=rgb_noise_reduced data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=B1_noise_rgb_effe_dice
-# python model=deeplabv3plus_efficientnet_b3 data=spectral_single data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=C1_spectral_effe_dice
-# python model=deeplabv3plus_efficientnet_b3 data=temporal data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=C1_temporal_effe_dice
+# python scripts/train.py  model=deeplabv3plus_efficientnet_b3 data=rgb_noise_reduced data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=B1_noise_rgb_effe_dice
+# python scripts/train.py  model=deeplabv3plus_efficientnet_b3 data=spectral_single data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=C1_spectral_effe_dice
+# python scripts/train.py  model=deeplabv3plus_efficientnet_b3 data=temporal data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=C1_temporal_effe_dice
 
 
