64c4dbe3ede891070fdbedf898c8a805ceee7c60
OisheeBintey_Hoque <bintu3003@gmail.com.com>
2026-03-19 06:54:42 -0400

    gitignore update

--- slurm_job.sh
+++ slurm_job.sh
@@ -21,9 +21,12 @@ export WANDB_CACHE_DIR=/sfs/weka/scratch/gza5dr/wandb_cache
 export WANDB_DATA_DIR=/sfs/weka/scratch/gza5dr/wandb_data
 
 
-python scripts/train.py model=deeplabv3plus_efficientnet_b3 data=rgb_single experiment_name=A4_dlv3_50_rgb_effe_dice_norm
-# python scripts/train.py  model=deeplabv3plus_efficientnet_b3 data=rgb_noise_reduced data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=B1_noise_rgb_effe_dice
-# python scripts/train.py  model=deeplabv3plus_efficientnet_b3 data=spectral_single data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=C1_spectral_effe_dice
-# python scripts/train.py  model=deeplabv3plus_efficientnet_b3 data=temporal data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=C1_temporal_effe_dice
+# python scripts/train.py model=deeplabv3plus_swin_tiny data=rgb_single experiment_name=A4_dlv3_50_rgb_effe_dice_norm
+
+
+# python scripts/train.py model=deeplabv3plus_resnet50 data=rgb_single experiment_name= DLR_50_rgb_effe_dice_norm
+# python scripts/train.py  model=deeplabv3plus_resnet50 data=rgb_noise_reduced data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=DLR_50_noise_rgb_effe_dice
+# python scripts/train.py  model=deeplabv3plus_resnet50 data=spectral_single data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=DLR_50_spectral_effe_dice
+python scripts/train.py  model=deeplabv3plus_resnet50 data=temporal data.noise_strategy=ndvi_relabel data.low_threshold=0.15 experiment_name=DLR_50_temporal_effe_dice
 
 
